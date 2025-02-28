b029cb1879c804d6bdc10bfb64b827c2f9be7550
Maxim <maxim.trz@gmail.com>
2025-02-28 15:37:35 +0500

    1

--- app/MoonShine/Pages/Dashboard.php
+++ app/MoonShine/Pages/Dashboard.php
@@ -4,8 +4,22 @@ declare(strict_types=1);
 
 namespace App\MoonShine\Pages;
 
+use App\Models\Test;
+use App\Models\TestResult;
+use App\MoonShine\Resources\TestGroupResource;
 use MoonShine\Laravel\Pages\Page;
 use MoonShine\Contracts\UI\ComponentContract;
+use MoonShine\UI\Components\Heading;
+use MoonShine\UI\Components\Layout\Box;
+use MoonShine\UI\Components\Layout\Column;
+use MoonShine\UI\Components\Layout\Divider;
+use MoonShine\UI\Components\Layout\Grid;
+use MoonShine\UI\Components\Layout\LineBreak;
+use MoonShine\UI\Components\Metrics\Wrapped\ValueMetric;
+use MoonShine\UI\Components\Table\TableBuilder;
+use MoonShine\UI\Fields\Preview;
+use MoonShine\UI\Fields\Text;
+
 
 class Dashboard extends Page
 {
@@ -21,7 +35,7 @@ class Dashboard extends Page
 
     public function getTitle(): string
     {
-        return $this->title ?: 'Dashboard';
+        return $this->title ?: 'Главная';
     }
 
     /**
@@ -29,6 +43,44 @@ class Dashboard extends Page
      */
     protected function components(): iterable
 	{
-		return [];
+        return [
+
+            Grid::make([
+                Column::make(
+                    [
+                        ValueMetric::make('Всего тестов')->value(Test::query()->where('teacher_id', auth()->id())->count())->columnSpan(6),
+                    ],
+                    colSpan: 6,
+                    adaptiveColSpan: 6
+                ),
+                Column::make(
+                    [
+                        ValueMetric::make('Выполнено тестов')->value(TestResult::query()->where('teacher_id', auth()->id())->count())->columnSpan(6),
+                    ],
+                    colSpan: 6,
+                    adaptiveColSpan: 6
+                ),
+            ]),
+
+
+
+            LineBreak::make(),
+
+            Grid::make([
+                Column::make(
+                    [
+                        TableBuilder::make()
+                            ->items(Test::query()->where('teacher_id', auth()->id())->limit(10)
+                                ->get()->toArray())
+                            ->fields([
+                                Text::make('Последние созданные тесты', 'title'),
+                            ]),
+                    ],
+                    colSpan: 12,
+                    adaptiveColSpan: 12
+                ),
+
+            ])
+        ];
 	}
 }
